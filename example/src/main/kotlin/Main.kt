71e85572332c2d1246f10f8726ff5f3e693eae69
Nils Jäkel <nils.jaekel@redtronics.dev>
2024-10-25 13:25:58 +0200

    feat: added auth grant example

--- example/src/main/kotlin/Main.kt
+++ example/src/main/kotlin/Main.kt
@@ -1,22 +1,21 @@
 package dev.redtronics
 
+import dev.redtronics.mokt.MSTenant
 import dev.redtronics.mokt.Microsoft
 import dev.redtronics.mokt.auth
+import io.ktor.http.*
 import kotlinx.coroutines.supervisorScope
 
 suspend fun main(): Unit = supervisorScope {
     auth<Microsoft> {
+        tenant = MSTenant.CONSUMERS
         clientId = "aa54f7f4-45f6-4f78-b95f-3b6cc98e0b7f"
 
-        device {
-            val code = requestAuthorizationCode()
+        codeGrant {
+            localRedirectUrl = Url("http://localhost:59187/callback")
 
-            displayCode(code!!.userCode) {
-                displayUserCodeInBrowser()
-            }
-
-            val accessResponse = requestAccessToken(code)
-            println(accessResponse?.accessToken)
+            val code = requestGrantCode()
+            requestAccessToken(code)
         }
     }
 }
