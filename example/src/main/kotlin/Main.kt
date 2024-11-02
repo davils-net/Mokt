8510e2961b95784a9d0dd2bcc959844025272a5b
Nils Jäkel <nils.jaekel@redtronics.dev>
2024-11-02 20:56:25 +0100

    refactor: removed old test code

--- example/src/main/kotlin/Main.kt
+++ example/src/main/kotlin/Main.kt
@@ -1,13 +1,7 @@
 package dev.redtronics
 
-import dev.redtronics.mokt.keycloakAuth
 import kotlinx.coroutines.supervisorScope
 
 suspend fun main(): Unit = supervisorScope {
-    keycloakAuth {
-        domain = "redtronics.dev"
-        realm = "huebcraft"
 
-
-    }
 }
