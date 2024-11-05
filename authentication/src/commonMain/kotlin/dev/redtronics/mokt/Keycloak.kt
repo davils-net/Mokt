eb9c195578704e24c5ce2e8f4c129f34f702eced
Nils Jäkel <nils.jaekel@redtronics.dev>
2024-11-05 11:36:23 +0100

    feat: return last value from function call

--- authentication/src/commonMain/kotlin/dev/redtronics/mokt/Keycloak.kt
+++ authentication/src/commonMain/kotlin/dev/redtronics/mokt/Keycloak.kt
@@ -14,11 +14,12 @@ package dev.redtronics.mokt
 import dev.redtronics.mokt.builder.device.KeycloakDeviceBuilder
 import dev.redtronics.mokt.network.client
 import dev.redtronics.mokt.network.defaultJson
+import dev.redtronics.mokt.network.div
 import io.ktor.client.*
 import io.ktor.http.*
 import kotlinx.serialization.json.Json
 
-public class Keycloak internal constructor() : Provider {
+public class Keycloak internal constructor() : Provider() {
     override val name: String
         get() = "Keycloak"
 
@@ -28,14 +29,19 @@ public class Keycloak internal constructor() : Provider {
 
     override var clientId: String? = getEnv("KEYCLOAK_CLIENT_ID")
 
-    override val tokenEndpointUrl: Url
-        get() = TODO("Not yet implemented")
-
     public var instanceUrl: Url? = null
+
+    override val tokenEndpointUrl: Url
+        get() = instanceUrl!! / "/realms/$realm/protocol/openid-connect/token"
 
     public var realm: String? = null
 
-    public suspend fun device(builder: suspend KeycloakDeviceBuilder.() -> Unit) {
-        KeycloakDeviceBuilder(this).apply { builder() }
+    public suspend fun <T> device(builder: suspend KeycloakDeviceBuilder.() -> T): T {
+        val keycloakBuilder = KeycloakDeviceBuilder(this)
+        return builder(keycloakBuilder).apply { build() }
+    }
+
+    override suspend fun build() {
+        require(clientId != null && realm != null && instanceUrl != null) { "You need to set client id, realm and instance url" }
     }
 }
