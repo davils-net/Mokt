baabd11756ca2d9a6b6c012f5e9c474d9d0a7aa5
Nils Jäkel <nils.jaekel@redtronics.dev>
2024-11-06 10:50:25 +0100

    docs: added docstring and added some func overloading for microsoft and keycloak

--- authentication/src/commonMain/kotlin/dev/redtronics/mokt/Provider.kt
+++ authentication/src/commonMain/kotlin/dev/redtronics/mokt/Provider.kt
@@ -52,19 +52,60 @@ public abstract class Provider {
      * */
     public abstract var json: Json
 
+    /**
+     * The url where the access token can be fetched.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
     public abstract val tokenEndpointUrl: Url
 
+    /**
+     * The client id of the authentication provider and is required to start the authentication flow.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
     public abstract var clientId: String?
 
+    /**
+     * The client secret of the authentication provider.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
     public abstract var clientSecret: String?
 
+    /**
+     * The scopes there are used to get the access token.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     *
+     * @see Scope
+     * */
     public var scopes: List<Scope> = Scope.allScopes
 
+    /**
+     * Requests the access token from the refresh token to renew the expired access token.
+     *
+     * @param refreshToken The refresh token from the expired access token.
+     * @param onRequestError The function to be called if an error occurs during the renewal of the access token.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
     public abstract suspend fun requestAccessTokenFromRefreshToken(
-        accessResponse: AccessResponse,
+        refreshToken: String,
         onRequestError: suspend (response: HttpResponse) -> Unit = {}
     ): AccessResponse?
 
+    /**
+     * Builds and validates the configuration of the auth provider.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
     internal abstract suspend fun build()
 }
 
@@ -84,25 +125,35 @@ public suspend fun microsoftAuth(builder: suspend Microsoft.() -> Unit): Microso
 }
 
 /**
- * Central adapter to communicate with the Mojang authentication provider.
- * It creates an instance of [Mojang] and configures it with the provided builder.
+ * Central adapter to communicate with the Microsoft authentication provider.
+ * It creates an instance of [Microsoft] and configures it with the provided builder.
  *
- * This provider is useful if you write your own authentication provider to
- * get the microsoft access token.
- * It means this provider cannot get the microsoft access token in any way.
- * You must implement this with your own provider.
+ * @param clientId The client id of the authentication provider.
+ * @param clientSecret The client secret of the authentication provider.
+ * @param tenant The tenant of the authentication provider to specify which users can sign in to the application.
+ * @param httpClient The http client used by the authentication provider.
+ * @param json The json serializer used by the authentication provider.
  *
- *
- * @param builder The builder to configure the [Microsoft] instance.
  * @return The initialized and configured [Microsoft] instance.
  *
  * @since 0.0.1
  * @author Nils Jäkel
+ *
+ * @see Tenant
  */
-//public suspend fun mojangAuth(builder: suspend Mojang.() -> Unit): Mojang {
-//    val mojang = Mojang().apply { builder() }
-//    return mojang
-//}
+public suspend fun microsoftAuth(
+    clientId: String,
+    clientSecret: String? = null,
+    tenant: Tenant = Tenant.CONSUMERS,
+    httpClient: HttpClient = client,
+    json: Json = defaultJson
+): Microsoft = microsoftAuth {
+    this.clientId = clientId
+    this.clientSecret = clientSecret
+    this.tenant = tenant
+    this.httpClient = httpClient
+    this.json = json
+}
 
 /**
  * Central adapter to communicate with the Keycloak authentication provider.
@@ -119,14 +170,32 @@ public suspend fun keycloakAuth(builder: suspend Keycloak.() -> Unit): Keycloak 
     return keycloak
 }
 
+/**
+ * Central adapter to communicate with the Keycloak authentication provider.
+ * It creates an instance of [Keycloak] and configures it with the provided builder.
+ *
+ * @param clientId The client id of the authentication provider.
+ * @param instanceUrl The base url of the keycloak instance.
+ * @param realm The realm of the keycloak instance, that should be used.
+ * @param clientSecret The client secret of the authentication provider.
+ * @param httpClient The http client used by the authentication provider.
+ * @param json The json serializer used by the authentication provider.
+ *
+ * @return The initialized and configured [Keycloak] instance.
+ *
+ * @since 0.0.1
+ * @author Nils Jäkel
+ * */
 public suspend fun keycloakAuth(
     clientId: String,
     instanceUrl: Url,
     realm: String,
+    clientSecret: String? = null,
     httpClient: HttpClient = client,
     json: Json = defaultJson
 ): Keycloak = keycloakAuth {
     this.clientId = clientId
+    this.clientSecret = clientSecret
     this.instanceUrl = instanceUrl
     this.realm = realm
     this.httpClient = httpClient
