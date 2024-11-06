e901417634ea712e9e21eb9a608ef78a898314fb
Nils Jäkel <nils.jaekel@redtronics.dev>
2024-11-06 09:52:24 +0100

    feat: added func to get the ms token response from the keycloak auth provider

--- authentication/src/commonMain/kotlin/dev/redtronics/mokt/Keycloak.kt
+++ authentication/src/commonMain/kotlin/dev/redtronics/mokt/Keycloak.kt
@@ -9,13 +9,18 @@
  * and/or sell copies of the Software.
  */
 
+@file:Suppress("MemberVisibilityCanBePrivate")
+
 package dev.redtronics.mokt
 
 import dev.redtronics.mokt.builder.device.KeycloakDeviceBuilder
 import dev.redtronics.mokt.network.client
 import dev.redtronics.mokt.network.defaultJson
 import dev.redtronics.mokt.network.div
+import dev.redtronics.mokt.response.AccessResponse
 import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import kotlinx.serialization.json.Json
 
@@ -38,10 +43,32 @@ public class Keycloak internal constructor() : Provider() {
     override val tokenEndpointUrl: Url
         get() = instanceUrl!! / "/realms/$realm/protocol/openid-connect/token"
 
+    public val microsoftTokenEndpoint: Url
+        get() = instanceUrl!! / "/realms/$realm/broker/microsoft/token"
+
 
     public suspend fun <T> device(builder: suspend KeycloakDeviceBuilder.() -> T): T {
         val keycloakBuilder = KeycloakDeviceBuilder(this)
         return builder(keycloakBuilder).apply { build() }
+    }
+
+    public suspend fun requestMicrosoftAccessToken(
+        accessResponse: AccessResponse,
+        onRequestError: suspend (response: HttpResponse) -> Unit = {}
+    ): AccessResponse? {
+        val response = httpClient.get(microsoftTokenEndpoint) {
+            headers {
+                contentType(ContentType.Application.Json)
+                bearerAuth(accessResponse.accessToken)
+            }
+        }
+
+        if (!response.status.isSuccess()) {
+            onRequestError(response)
+            return null
+        }
+
+        return json.decodeFromString(AccessResponse.serializer(), response.bodyAsText())
     }
 
     override suspend fun build() {
