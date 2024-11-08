378ec821bbd0deaf705bec5c091d6a62cfab341f
lars <lcu4android@gmail.com>
2024-11-08 20:54:40 +0100

    feat: Impl get env

--- common/src/tvosMain/kotlin/dev/redtronics/mokt/System.tvos.kt
+++ common/src/tvosMain/kotlin/dev/redtronics/mokt/System.tvos.kt
@@ -11,8 +11,15 @@
 
 package dev.redtronics.mokt
 
+import dev.redtronics.mokt.cinterop.get_env
+import kotlinx.cinterop.toKString
+
 public actual fun getEnv(key: String): String? {
-    TODO("Not yet implemented")
+    val value = get_env(key)?.toKString()
+    if (value.isNullOrEmpty()) {
+        return null
+    }
+    return value
 }
 
 public actual val os: OsType = OsType.TVOS
