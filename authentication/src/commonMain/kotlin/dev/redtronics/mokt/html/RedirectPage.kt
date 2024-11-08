79f2fa813dde25c1997f5eba9226563a34873c03
Nils Jäkel <nils.jaekel@redtronics.dev>
2024-11-08 12:10:44 +0100

    fix: warning operator overloading

--- authentication/src/commonMain/kotlin/dev/redtronics/mokt/html/RedirectPage.kt
+++ authentication/src/commonMain/kotlin/dev/redtronics/mokt/html/RedirectPage.kt
@@ -18,76 +18,78 @@ public fun HTML.redirectPage(
     firstAccentColor: String,
     secondAccentColor: String,
     title: String,
-    notice: String
+    notice: String,
 ) {
     head {
         title(title)
 
         style {
-            //language=CSS
-            +"""
-            body {
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                height: 100vh;
-                margin: 0;
-                color: $textColor;
-                font-family: 'Arial', sans-serif;
-                overflow: hidden;
-                position: relative;
-                background: linear-gradient(45deg, $firstAccentColor, $secondAccentColor);
-                background-size: 400% 400%;
-                animation: gradientAnimation 20s infinite;
-                text-align: center;
-            }
-
-            @keyframes gradientAnimation {
-                0% {
-                    background-position: 0 50%;
+            unsafe {
+                //language=CSS
+                +"""
+                body {
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    height: 100vh;
+                    margin: 0;
+                    color: $textColor;
+                    font-family: 'Arial', sans-serif;
+                    overflow: hidden;
+                    position: relative;
+                    background: linear-gradient(45deg, $firstAccentColor, $secondAccentColor);
+                    background-size: 400% 400%;
+                    animation: gradientAnimation 20s infinite;
+                    text-align: center;
                 }
-                50% {
-                    background-position: 100% 50%;
+    
+                @keyframes gradientAnimation {
+                    0% {
+                        background-position: 0 50%;
+                    }
+                    50% {
+                        background-position: 100% 50%;
+                    }
+                    100% {
+                        background-position: 0 50%;
+                    }
+    
+                .container {
+                    text-align: center;
+                    transform: translateY(-50px);
+                    animation: fadeInUp 1.5s ease-out forwards;
+                    z-index: 1;
                 }
-                100% {
-                    background-position: 0 50%;
+    
+                h1 {
+                    font-size: 3em;
+                    font-weight: bold;
+                    animation: textSlideIn 2s ease-out forwards;
                 }
-
-            .container {
-                text-align: center;
-                transform: translateY(-50px);
-                animation: fadeInUp 1.5s ease-out forwards;
-                z-index: 1;
-            }
-
-            h1 {
-                font-size: 3em;
-                font-weight: bold;
-                animation: textSlideIn 2s ease-out forwards;
-            }
-
-            p {
-                font-size: 1.2em;
-                margin-top: 20px;
-            }
-
-            @keyframes fadeInUp {
-                to {
-                    transform: translateY(0);
+    
+                p {
+                    font-size: 1.2em;
+                    margin-top: 20px;
                 }
-            }
-
-            @keyframes textSlideIn {
-                from {
-                    transform: translateY(-20px);
-                    opacity: 0;
+    
+                @keyframes fadeInUp {
+                    to {
+                        transform: translateY(0);
+                    }
                 }
-                to {
-                    transform: translateY(0);
-                    opacity: 1;
+    
+                @keyframes textSlideIn {
+                    from {
+                        transform: translateY(-20px);
+                        opacity: 0;
+                    }
+                    to {
+                        transform: translateY(0);
+                        opacity: 1;
+                    }
                 }
+                """.trimIndent()
             }
-            """.trimIndent()
         }
     }
 
