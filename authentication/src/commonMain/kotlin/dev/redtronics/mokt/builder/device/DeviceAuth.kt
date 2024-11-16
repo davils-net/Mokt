585942e44ff731dba875021df987fbd541ec0139
Nils Jäkel <nils.jaekel@redtronics.dev>
2024-11-16 21:42:29 +0100

    feat: added flow support for device auth

--- authentication/src/commonMain/kotlin/dev/redtronics/mokt/builder/device/DeviceAuth.kt
+++ authentication/src/commonMain/kotlin/dev/redtronics/mokt/builder/device/DeviceAuth.kt
@@ -19,6 +19,7 @@ import dev.redtronics.mokt.OAuth
 import dev.redtronics.mokt.Provider
 import dev.redtronics.mokt.build.BuildConstants
 import dev.redtronics.mokt.builder.device.code.UserCodeBuilder
+import dev.redtronics.mokt.flows.*
 import dev.redtronics.mokt.html.WebTheme
 import dev.redtronics.mokt.html.userCodePage
 import dev.redtronics.mokt.network.interval
@@ -34,6 +35,8 @@ import io.ktor.http.*
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
 import io.ktor.util.date.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
 import kotlinx.html.HTML
 import kotlin.time.Duration.Companion.seconds
 
@@ -72,7 +75,10 @@ public abstract class DeviceAuth<out T : Provider> internal constructor() : OAut
      * @since 0.0.1
      * @author Nils Jäkel
      * */
-    public suspend fun displayCode(deviceCodeResponse: DeviceCodeResponse, builder: suspend UserCodeBuilder.() -> Unit) {
+    public suspend fun displayCode(
+        deviceCodeResponse: DeviceCodeResponse,
+        builder: suspend UserCodeBuilder.() -> Unit,
+    ) {
         val userCodeBuilder = UserCodeBuilder(deviceCodeResponse).apply { builder() }
         codeServer = userCodeBuilder.build()
     }
@@ -102,7 +108,17 @@ public abstract class DeviceAuth<out T : Provider> internal constructor() : OAut
         logoDescription: String = "Mokt logo",
         backgroundUrl: Url = Url(BuildConstants.MOKT_DEVICE_CODE_BACKGROUND),
         userCodeHint: String = "Enter the code below in your browser",
-        page: HTML.(userCode: String) -> Unit = { userCode -> userCodePage(userCode, title, logoUrl, logoDescription, backgroundUrl, userCodeHint, theme) },
+        page: HTML.(userCode: String) -> Unit = { userCode ->
+            userCodePage(
+                userCode,
+                title,
+                logoUrl,
+                logoDescription,
+                backgroundUrl,
+                userCodeHint,
+                theme
+            )
+        },
     ): Unit = displayCode(deviceCodeResponse) {
         inBrowser {
             this.title = title
@@ -131,14 +147,45 @@ public abstract class DeviceAuth<out T : Provider> internal constructor() : OAut
         deviceCodeResponse: DeviceCodeResponse,
         title: String = "Device Code",
         userCodeHint: String = "Enter the code below in your browser",
-        screen: suspend Terminal.(userCode: String) -> Unit = { userCode -> userCodeScreen(userCode, title, userCodeHint, Url(deviceCodeResponse.verificationUri)) },
-        terminal: Terminal = Terminal()
+        screen: suspend Terminal.(userCode: String) -> Unit = { userCode ->
+            userCodeScreen(
+                userCode,
+                title,
+                userCodeHint,
+                Url(deviceCodeResponse.verificationUri)
+            )
+        },
+        terminal: Terminal = Terminal(),
     ): Unit = displayCode(deviceCodeResponse) {
         inTerminal {
             this.title = title
             this.userCodeHint = userCodeHint
             this.screen = screen
             this.terminal = terminal
+        }
+    }
+
+    /**
+     * Displays the user code as [Flow].
+     *
+     * @param builder The builder to configure the user code display.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
+    public fun <T : DeviceAuthData> displayCode(
+        builder: suspend UserCodeBuilder.() -> Unit,
+    ): FlowStep<T, AuthProgress<OAuthState>> = object : FlowStep<T, AuthProgress<OAuthState>> {
+        override suspend fun execute(flowData: T): Flow<AuthProgress<OAuthState>> = channelFlow {
+            send(AuthProgress(1, 2, OAuthState.DISPLAY_USER_CODE))
+            if (flowData.deviceCodeResponse == null) {
+                send(AuthProgress(2, 2, OAuthState.DISPLAY_USER_CODE))
+                return@channelFlow
+            }
+
+            val deviceCodeResponse = flowData.deviceCodeResponse!!
+            displayCode(deviceCodeResponse, builder)
+            send(AuthProgress(2, 2, OAuthState.DISPLAY_USER_CODE))
         }
     }
 
@@ -153,8 +200,59 @@ public abstract class DeviceAuth<out T : Provider> internal constructor() : OAut
      * */
     public suspend fun authorizationCode(
         additionalParameters: Map<String, String> = mapOf(),
-        onRequestError: suspend (err: CodeErrorResponse) -> Unit = {}
+        onRequestError: suspend (err: CodeErrorResponse) -> Unit = {},
     ): DeviceCodeResponse? {
+        val response = submitAuthorizationForm(additionalParameters)
+        if (!response.status.isSuccess()) {
+            onRequestError(provider.json.decodeFromString(CodeErrorResponse.serializer(), response.bodyAsText()))
+            return null
+        }
+        return provider.json.decodeFromString(DeviceCodeResponse.serializer(), response.bodyAsText())
+    }
+
+    /**
+     * Requests the authorization code from the device code endpoint as [Flow].
+     *
+     * @param additionalParameters The additional parameters to be appended to the request.
+     * @param onRequestError The function to be called if an error occurs during the authorization code request.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
+    public fun <T : DeviceAuthData> authorizationCode(
+        additionalParameters: Map<String, String> = mapOf(),
+        onRequestError: suspend (err: CodeErrorResponse, flowData: T) -> Unit = { _, flowData -> flowData.cancel() },
+    ): FlowStep<T, AuthProgress<OAuthState>> = object : FlowStep<T, AuthProgress<OAuthState>> {
+        override suspend fun execute(flowData: T): Flow<AuthProgress<OAuthState>> = channelFlow {
+            send(AuthProgress(currentStep = 1, totalSteps = 2, state = OAuthState.REQUEST_USER_CODE))
+
+            val response = submitAuthorizationForm(additionalParameters)
+            if (!response.status.isSuccess()) {
+                send(AuthProgress(currentStep = 2, totalSteps = 2, state = OAuthState.REQUEST_USER_CODE))
+                onRequestError(
+                    provider.json.decodeFromString(CodeErrorResponse.serializer(), response.bodyAsText()),
+                    flowData
+                )
+                return@channelFlow
+            }
+
+            val code = provider.json.decodeFromString(DeviceCodeResponse.serializer(), response.bodyAsText())
+            flowData.deviceCodeResponse = code
+            send(AuthProgress(currentStep = 2, totalSteps = 2, state = OAuthState.REQUEST_USER_CODE))
+        }
+    }
+
+    /**
+     * Submits the authorization form to the device code endpoint.
+     *
+     * @param additionalParameters The additional parameters to be appended to the request.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
+    private suspend fun submitAuthorizationForm(
+        additionalParameters: Map<String, String> = mapOf(),
+    ): HttpResponse {
         val response = provider.httpClient.submitForm(
             url = deviceCodeEndpointUrl.toString(),
             formParameters = parameters {
@@ -170,11 +268,7 @@ public abstract class DeviceAuth<out T : Provider> internal constructor() : OAut
                 }
             }
         )
-        if (!response.status.isSuccess()) {
-            onRequestError(provider.json.decodeFromString(CodeErrorResponse.serializer(), response.bodyAsText()))
-            return null
-        }
-        return provider.json.decodeFromString(DeviceCodeResponse.serializer(), response.bodyAsText())
+        return response
     }
 
     /**
@@ -190,7 +284,7 @@ public abstract class DeviceAuth<out T : Provider> internal constructor() : OAut
     public suspend fun accessToken(
         deviceCodeResponse: DeviceCodeResponse,
         additionalParameters: Map<String, String> = mapOf(),
-        onRequestError: suspend (err: DeviceAuthStateError) -> Unit = {}
+        onRequestError: suspend (err: DeviceAuthStateError) -> Unit = {},
     ): AccessResponse? {
         val startTime = getTimeMillis()
         return authLoop(startTime, deviceCodeResponse, additionalParameters, onRequestError)
@@ -210,10 +304,70 @@ public abstract class DeviceAuth<out T : Provider> internal constructor() : OAut
         deviceCodeResponse: DeviceCodeResponse,
         additionalParameters: Map<String, String> = mapOf(),
         displayUserCode: suspend UserCodeBuilder.() -> Unit = {},
-        onRequestError: suspend (err: DeviceAuthStateError) -> Unit = {}
+        onRequestError: suspend (err: DeviceAuthStateError) -> Unit = {},
     ): AccessResponse? {
         displayCode(deviceCodeResponse, displayUserCode)
         return accessToken(deviceCodeResponse, additionalParameters, onRequestError)
+    }
+
+    /**
+     * Requests the access token from the device code endpoint as [Flow].
+     *
+     * @param additionalParameters The additional parameters to be appended to the request.
+     * @param onRequestError The function to be called if an error occurs during the access token request.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
+    public fun <T : DeviceAuthData> accessToken(
+        additionalParameters: Map<String, String> = mapOf(),
+        onRequestError: suspend (err: DeviceAuthStateError, flowData: T) -> Unit = { _, flowData -> flowData.cancel() },
+    ): FlowStep<T, AuthProgress<OAuthState>> = object : FlowStep<T, AuthProgress<OAuthState>> {
+        override suspend fun execute(flowData: T): Flow<AuthProgress<OAuthState>> = channelFlow {
+            send(AuthProgress(1, 2, OAuthState.REQUEST_ACCESS_TOKEN))
+            if (flowData.deviceCodeResponse == null) {
+                send(AuthProgress(2, 2, OAuthState.REQUEST_ACCESS_TOKEN))
+                return@channelFlow
+            }
+
+            val deviceCodeResponse = flowData.deviceCodeResponse!!
+            val startTime = getTimeMillis()
+            val accessResponse = authLoop(startTime, deviceCodeResponse, additionalParameters, flowData, onRequestError)
+
+            flowData.accessResponse = accessResponse
+            send(AuthProgress(2, 2, OAuthState.REQUEST_ACCESS_TOKEN))
+        }
+    }
+
+    /**
+     * Requests the access token from the device code endpoint as [Flow].
+     *
+     * @param additionalParameters The additional parameters to be appended to the request.
+     * @param onRequestError The function to be called if an error occurs during the access token request.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
+    public fun <T : DeviceAuthData> accessToken(
+        additionalParameters: Map<String, String> = mapOf(),
+        displayUserCode: suspend UserCodeBuilder.() -> Unit = {},
+        onRequestError: suspend (err: DeviceAuthStateError, flowData: T) -> Unit = { _, flowData -> flowData.cancel() },
+    ): FlowStep<T, AuthProgress<OAuthState>> = object : FlowStep<T, AuthProgress<OAuthState>>{
+        override suspend fun execute(flowData: T): Flow<AuthProgress<OAuthState>> = channelFlow {
+            send(AuthProgress(1, 2, OAuthState.REQUEST_ACCESS_TOKEN))
+            if (flowData.deviceCodeResponse == null) {
+                send(AuthProgress(2, 2, OAuthState.REQUEST_ACCESS_TOKEN))
+                return@channelFlow
+            }
+
+            val deviceCodeResponse = flowData.deviceCodeResponse!!
+            displayCode(deviceCodeResponse, displayUserCode)
+            val startTime = getTimeMillis()
+            val accessResponse = authLoop(startTime, deviceCodeResponse, additionalParameters, flowData, onRequestError)
+
+            flowData.accessResponse = accessResponse
+            send(AuthProgress(2, 2, OAuthState.REQUEST_ACCESS_TOKEN))
+        }
     }
 
     /**
@@ -227,15 +381,82 @@ public abstract class DeviceAuth<out T : Provider> internal constructor() : OAut
      * @since 0.0.1
      * @author Nils Jäkel
      * */
-    internal suspend fun authLoop(
+    private suspend fun authLoop(
         startTime: Long,
         deviceCodeResponse: DeviceCodeResponse,
         additionalParameters: Map<String, String>,
-        onRequestError: suspend (err: DeviceAuthStateError) -> Unit
+        onRequestError: suspend (err: DeviceAuthStateError) -> Unit,
     ): AccessResponse? = interval(
         interval = deviceCodeResponse.interval.seconds,
         cond = { getTimeMillis() - startTime < deviceCodeResponse.expiresIn * 1000 }
     ) {
+        val response = submitAuthLoopForm(deviceCodeResponse, additionalParameters)
+        val responseBody = response.bodyAsText()
+
+        if (responseBody.contains("error")) {
+            val errorResponse = provider.json.decodeFromString(DeviceAuthStateError.serializer(), responseBody)
+            if (errorResponse.error != DeviceAuthStateErrorItem.AUTHORIZATION_PENDING) {
+                onRequestError(errorResponse)
+                cancel()
+            }
+            return@interval null
+        }
+
+        codeServer?.stop()
+        return@interval provider.json.decodeFromString(AccessResponse.serializer(), responseBody)
+    }
+
+    /**
+     * Interval to poll for the access token from the device token endpoint with [Flow] error handling.
+     *
+     * @param startTime The current start time.
+     * @param deviceCodeResponse The device code response.
+     * @param additionalParameters The additional parameters to be appended to the request.
+     * @param flowData The flow data.
+     * @param onRequestError The function to be called if an error occurs during the access token request.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
+    private suspend fun <T : DeviceAuthData> authLoop(
+        startTime: Long,
+        deviceCodeResponse: DeviceCodeResponse,
+        additionalParameters: Map<String, String>,
+        flowData: T,
+        onRequestError: suspend (err: DeviceAuthStateError, flowData: T) -> Unit = { _, _ -> },
+    ): AccessResponse? = interval(
+        interval = deviceCodeResponse.interval.seconds,
+        cond = { getTimeMillis() - startTime < deviceCodeResponse.expiresIn * 1000 }
+    ) {
+        val response = submitAuthLoopForm(deviceCodeResponse, additionalParameters)
+        val responseBody = response.bodyAsText()
+
+        if (responseBody.contains("error")) {
+            val errorResponse = provider.json.decodeFromString(DeviceAuthStateError.serializer(), responseBody)
+            if (errorResponse.error != DeviceAuthStateErrorItem.AUTHORIZATION_PENDING) {
+                onRequestError(errorResponse, flowData)
+                cancel()
+            }
+            return@interval null
+        }
+
+        codeServer?.stop()
+        return@interval provider.json.decodeFromString(AccessResponse.serializer(), responseBody)
+    }
+
+    /**
+     * Submits the authorization form to the device code endpoint.
+     *
+     * @param deviceCodeResponse The device code response.
+     * @param additionalParameters The additional parameters to be appended to the request.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
+    private suspend fun submitAuthLoopForm(
+        deviceCodeResponse: DeviceCodeResponse,
+        additionalParameters: Map<String, String>,
+    ): HttpResponse {
         val response = provider.httpClient.submitForm(
             url = provider.tokenEndpointUrl.toString(),
             formParameters = parameters {
@@ -252,18 +473,6 @@ public abstract class DeviceAuth<out T : Provider> internal constructor() : OAut
                 }
             }
         )
-
-        val responseBody = response.bodyAsText()
-        if (responseBody.contains("error")) {
-            val errorResponse = provider.json.decodeFromString(DeviceAuthStateError.serializer(), responseBody)
-            if (errorResponse.error != DeviceAuthStateErrorItem.AUTHORIZATION_PENDING) {
-                onRequestError(errorResponse)
-                cancel()
-            }
-            return@interval null
-        }
-
-        codeServer?.stop()
-        return@interval provider.json.decodeFromString(AccessResponse.serializer(), responseBody)
+        return response
     }
 }
