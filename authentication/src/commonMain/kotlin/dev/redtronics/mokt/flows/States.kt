3811b9663bc79b63f1a357853510ce04f421cd26
Nils Jäkel <nils.jaekel@redtronics.dev>
2024-11-16 18:44:48 +0100

    feat: added oauth states and minecraft auth states

--- authentication/src/commonMain/kotlin/dev/redtronics/mokt/flows/States.kt
+++ authentication/src/commonMain/kotlin/dev/redtronics/mokt/flows/States.kt
@@ -15,18 +15,17 @@ public interface AuthState {
     public val description: String
 }
 
-public enum class DeviceAuthState(override val description: String) : AuthState {
+public enum class OAuthState(override val description: String) : AuthState {
     REQUEST_DEVICE_CODE("Request device code"),
+    REQUEST_GRANT_CODE("Request grant code"),
     DISPLAY_USER_CODE("Display user code"),
     AUTHORIZE_PENDING_USER_CODE("Authorize pending user code"),
-    REQUEST_ACCESS_TOKEN_FROM_AUTH_PROVIDER("Request access token from auth provider"),
-    REQUEST_ACCESS_TOKEN_FROM_MICROSOFT("Request access token from microsoft"),
-    REQUEST_REFRESH_TOKEN_FROM_MICROSOFT("Request refresh token from microsoft");
+    REQUEST_ACCESS_TOKEN("Request access token from microsoft"),
+    REQUEST_REFRESH_TOKEN("Request refresh token from microsoft");
 }
 
-public enum class GrantAuthState(override val description: String) : AuthState {
-    REQUEST_GRANT_CODE("Request auth code"),
-    REQUEST_ACCESS_TOKEN_FROM_AUTH_PROVIDER("Request access token from auth provider"),
-    REQUEST_ACCESS_TOKEN_FROM_MICROSOFT("Request access token from microsoft"),
-    REQUEST_REFRESH_TOKEN_FROM_MICROSOFT("Request refresh token from microsoft");
+public enum class MinecraftAuthState(override val description: String) : AuthState {
+    REQUEST_XBOX_ACCESS_TOKEN("Request XBox access token"),
+    REQUEST_XSTS_ACCESS_TOKEN("Request xsts access token"),
+    REQUEST_MOJANG_TOKEN("Request mojang access token");
 }
