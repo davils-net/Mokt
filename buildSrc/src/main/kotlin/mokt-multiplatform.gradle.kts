46cf4afb7d1b0b2a7e4840a0ba462291acc59641
Nils Jäkel <nils.jaekel@davils.net>
2024-12-19 14:53:23 +0100

    feat: add kreate gradle plugin

--- buildSrc/src/main/kotlin/mokt-multiplatform.gradle.kts
+++ buildSrc/src/main/kotlin/mokt-multiplatform.gradle.kts
@@ -9,88 +9,54 @@
  * and/or sell copies of the Software.
  */
 
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-import org.gradle.api.tasks.testing.logging.TestLogEvent
-import org.jetbrains.gradle.ext.IdeaExtPlugin
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import net.davils.kreate.feature.core.License
 
 plugins {
     org.jetbrains.kotlin.plugin.serialization
     org.jetbrains.kotlinx.atomicfu
     org.jetbrains.kotlin.multiplatform
-    io.kotest.multiplatform
-    org.jetbrains.dokka
+    net.davils.kreate
 }
-
-version =
-    System.getenv(/* name = */ "CI_COMMIT_TAG") ?: System.getenv(/* name = */ "CI_COMMIT_SHORT_SHA")?.let { "$it-dev" } ?: "0.0.0"
 
 repositories {
     mavenCentral()
 }
 
-if (!rootProject.pluginManager.hasPlugin("org.jetbrains.gradle.plugin.idea-ext")) {
-    rootProject.pluginManager.apply(IdeaExtPlugin::class)
-}
-
-val jvmTargetVersion = JvmTarget.JVM_1_8
-val targetJavaVersion = JavaVersion.VERSION_1_8
-val dokkaConfiguration = extensions.create("dokkaConfiguration", DokkaConfiguration::class)
-
-kotlin {
-    explicitApi()
-    withSourcesJar()
-
-    jvm {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        compilerOptions.jvmTarget = jvmTargetVersion
-    }
-}
-
-tasks {
-    register("patchVersion") {
-        group = Project.NAME
-        description = "Patches the docs and rust version."
-
-        doFirst {
-            project.patchVersion()
-        }
+kreate {
+    core {
+        name = Project.NAME
+        description = Project.DESCRIPTION
+        license = License.MIT
+        allWarningsAsErrors = true
+        isExplicitApiMode = true
     }
 
-    withType<JavaCompile> {
-        options.encoding = "UTF-8"
-        targetCompatibility = targetJavaVersion.toString()
-        sourceCompatibility = targetJavaVersion.toString()
+    publish {
+        enabled = true
+        inceptionYear = 2024
     }
 
-    withType<KotlinCompile> {
-        compilerOptions {
-            jvmTarget = jvmTargetVersion
-        }
+    testing {
+        enabled = true
+        createTestReport = true
     }
 
-    withType<Test> {
-        useJUnitPlatform()
-        reports.junitXml.required = true
-        systemProperty("gradle.build.dir", layout.buildDirectory.get().asFile.absolutePath)
-
-        filter {
-            isFailOnNoMatchingTests = false
-        }
-
-        testLogging {
-            showExceptions = true
-            showStandardStreams = true
-            events = setOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
-            exceptionFormat = TestExceptionFormat.FULL
-        }
+    docs {
+        enabled = true
+        isMultiModuleMode = true
     }
 
-    dokkaHtml {
-        moduleName = dokkaConfiguration.name.getOrElse(Project.NAME)
-        description = dokkaConfiguration.description.getOrElse(Project.DESCRIPTION)
-        failOnWarning = true
+    buildConstants {
+        enabled = true
+        buildPath = "generated/templates"
+        onlyInternal = true
+        sourceSets = kotlin.sourceSets.getByName("commonMain")
+    }
+
+    jv {
+        enabled = true
+        javaVersion = JavaVersion.VERSION_1_8
+        withJavadocJar = true
+        withSourcesJar = true
     }
 }
