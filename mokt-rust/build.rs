e6c82ce1de34272cbb5f7ae7a59d5beb841f1188
Nils Jäkel <nils.jaekel@davils.net>
2024-12-19 14:52:35 +0100

    refactor: use kreate cinterop feature

---
+++ mokt-rust/build.rs
@@ -0,0 +1,15 @@
+extern crate cbindgen;
+
+use cbindgen::Language::C;
+use std::env;
+
+fn main() {
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    cbindgen::Builder::new()
+        .with_crate(crate_dir)
+        .with_language(C)
+        .generate()
+        .expect("Unable to generate bindings")
+        .write_to_file("include/mokt.h");
+}
