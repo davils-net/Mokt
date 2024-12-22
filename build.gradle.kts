b0c623648cc7e9cba12bbfc48e08d93f2ba43369
Nils Jäkel <nils.jaekel@davils.net>
2024-12-22 12:13:46 +0100

    refactor: remove dokka plugin by my own

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,6 @@
  * and/or sell copies of the Software.
  */
 
-plugins {
-    org.jetbrains.dokka
-}
-
 version = System.getenv(/* name = */ "CI_COMMIT_TAG") ?: System.getenv(/* name = */ "CI_COMMIT_SHORT_SHA")?.let { "$it-dev" } ?: "0.0.0-DEV"
 
 repositories {
